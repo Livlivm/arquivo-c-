03c5544d6d4f4f9148ccc7c9a6cdc4677356de28
Livlivm <liviaampereira@gmail.com>
2025-06-03 11:37:31 -0300

    Add files via upload

---
+++ Sistema escolar/notas.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "alunos.h"
+#include "notas.h"
+
+void cadastrarNota(Nota *nota, int *conta){
+    printf("Cadastrar Nota\n");
+    printf("Informe a matricula do aluno :");
+    scanf("%d", &nota->matricula);
+    printf("Informa a nota :");
+    scanf("%f", &nota->nota);
+    *conta = *conta +1;
+}
+   
